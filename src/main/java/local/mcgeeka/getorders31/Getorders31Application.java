5e76ce84081b8f0d06eedd42657f2b5062d60b9d
Kate McGee <mcgeehee@gmail.com>
2020-07-14 22:27:16 -0600

    adding branch

---
+++ src/main/java/local/mcgeeka/getorders31/Getorders31Application.java
@@ -0,0 +1,16 @@
+package local.mcgeeka.getorders31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Getorders31Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Getorders31Application.class,
+            args);
+    }
+
+}
---
+++ src/test/java/local/mcgeeka/getorders31/Getorders31ApplicationTests.java
@@ -0,0 +1,15 @@
+package local.mcgeeka.getorders31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Getorders31ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
