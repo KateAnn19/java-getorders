9b015833f136f60af6ea1fae6c1d5fcf266adc3d
Kate McGee <mcgeehee@gmail.com>
2020-07-15 22:48:00 -0600

    added posts

--- src/main/java/local/mcgeeka/getorders31/controllers/CustomerController.java
+++ src/main/java/local/mcgeeka/getorders31/controllers/CustomerController.java
@@ -4,10 +4,14 @@ import local.mcgeeka.getorders31.models.Customer;
 import local.mcgeeka.getorders31.services.CustomerService;
 import local.mcgeeka.getorders31.views.CustomerOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -54,16 +58,67 @@ public class CustomerController
         return new ResponseEntity<>(myList, HttpStatus.OK);
     }
 
-        //    POST /customers/customer - Adds a new customer including any new orders
-        //    POST http://localhost:2019/customers/customer
 
-        //    PUT /customers/customer/{custcode} - completely replaces the customer record including associated orders with the provided data
-        //    PUT http://localhost:2019/customers/customer/19
+    //    POST /customers/customer - Adds a new customer including any new orders
+    //    POST http://localhost:2019/customers/customer
 
-        //    PATCH /customers/customer/{custcode} - updates customers with the new data. Only the new data is to be sent from the frontend client.
-        //    PATCH http://localhost:2019/customers/customer/19
+    //Create - POST
+    //Request Body - JSON Object New Restaurant -- in body will be new data to send over
+    //this is the most complicated thing inside the controller
+    //we create a new record in database
+    //an ID will be assigned
+    //We have to let client know how to find the new record (by id)
+    //we're bringing in a new JSON oject with this
+    //this actually consumes JSON -- so we use "consumes"
+    //you could list multiple types it can consume ... so you might see it in {} (a list)
+    //doesn't need @PathVariable with a POST ... needs @RequestBody
+    //Jackson converts data into an object
+    //@Valid is used to check that the incoming data is valid and throws and error if it isn't
+    //@Valid is not bulletproof but it's good
+    //CLIENT IS SENDING INFO BUT NOT SENDING ID
+    @PostMapping(value = "/customer", consumes = {"application/json"})
+    public ResponseEntity<?> addNewRestaurant(@Valid @RequestBody Customer newCust)
+    {
+        newCust.setCustcode(0); //this wipes out an id if the client sends a new object with an id (we don't want that)
+        //we already have a save method so we use it to save
+        newCust = customerService.save(newCust);
+        // in order to tell client how to access the newly created object
+        //location header sent back -- this below creates a location header
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newRestaurantURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{custcode}/").buildAndExpand(newCust.getCustcode()).toUri();
+        responseHeaders.setLocation(newRestaurantURI);   //this takes in a URI (required)
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+        //could return the new JSON object instead of null if you wanted to
+    }
 
-        //    DELETE /customers/customer/{custcode} - Deletes the given customer including any associated orders
+    //    PUT /customers/customer/{custcode} - completely replaces the customer record including associated orders with the provided data
+    //    PUT http://localhost:2019/customers/customer/19
+
+    //PUT
+    //we'll be changing the menu id's (though with a PUT you're really not 'supposed' to change any of the data
+    //We send it a Request Body and it must be complete because it's a complete replacement - JSON Object
+    @PutMapping(value = "/restaurant/{id}", consumes = "application/json")
+    public ResponseEntity<?> updateRestaurant(@PathVariable long id, @Valid @RequestBody Restaurant updateRest) // has two parameters and order doesn't matter
+    {
+        updateRest.setRestaurantid(id);
+        restaurantServices.save(updateRest);
+        return new ResponseEntity<>(updateRest,HttpStatus.OK);
+    }
+
+    //    PATCH /customers/customer/{custcode} - updates customers with the new data. Only the new data is to be sent from the frontend client.
+    //    PATCH http://localhost:2019/customers/customer/19
+
+    //PATCH
+    //brings in request body so needs consumes
+    @PatchMapping(value = "/restaurant/{restid}", consumes = "application/json")
+    public ResponseEntity<?> updatePartRestaurant(@RequestBody Restaurant updateRestaurant, @PathVariable long restid)
+    {
+        restaurantServices.update(updateRestaurant, restid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    //    DELETE /customers/customer/{custcode} - Deletes the given customer including any associated orders
         //    DELETE http://localhost:2019/customers/customer/54
         @DeleteMapping(value = "/customer/{custcode}")
         public ResponseEntity<?> deleteCustById(@PathVariable long custcode)
--- src/main/java/local/mcgeeka/getorders31/controllers/OrderController.java
+++ src/main/java/local/mcgeeka/getorders31/controllers/OrderController.java
@@ -1,11 +1,17 @@
 package local.mcgeeka.getorders31.controllers;
 
+import local.mcgeeka.getorders31.models.Customer;
 import local.mcgeeka.getorders31.models.Order;
 import local.mcgeeka.getorders31.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 //GET /orders/order/{id} - Returns the order and its customer with the given order number
 // GET /customers/orders/count - Using a custom query, return a list of all customers with the number of orders they have placed.
@@ -27,11 +33,22 @@ public class OrderController
     }
 
 
-
-
-
     //   POST /orders/order - adds a new order to an existing customer
     //    POST http://localhost:2019/orders/order
+    @PostMapping(value = "/order", consumes = {"application/json"})
+    public ResponseEntity<?> addNewRestaurant(@Valid @RequestBody Order newOrder)
+    {
+        newOrder.setOrdnum(0); //this wipes out an id if the client sends a new object with an id (we don't want that)
+        //we already have a save method so we use it to save
+        newOrder = ordersService.save(newOrder);
+        // in order to tell client how to access the newly created object
+        //location header sent back -- this below creates a location header
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newRestaurantURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{ordnum}/").buildAndExpand(newOrder.getOrdnum()).toUri();
+        responseHeaders.setLocation(newRestaurantURI);   //this takes in a URI (required)
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+        //could return the new JSON object instead of null if you wanted to
+    }
 
     //   PUT /orders/order/{ordernum} - completely replaces the given order
     //    PUT http://localhost:2019/orders/order/63
